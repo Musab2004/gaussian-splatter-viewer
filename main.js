d21276b09a6df24c0437e93758c01dc523f6ab9e
Musab <musabbinabaid2004@gmail.com>
2025-02-07 12:49:54 +0500

    just testing for now

--- main.js
+++ main.js
@@ -726,14 +726,16 @@ async function main() {
     viewMatrix = JSON.parse(decodeURIComponent(location.hash.slice(1)));
     carousel = false;
   } catch (err) {}
-  const fileId = "1Gl6VRqnT9M_xO9lmAt2VhZmKCA0fs5xs";
-  const url = `https://drive.google.com/uc?export=download&id=${fileId}`;
-
+  const url = new URL(
+    // "nike.splat",
+    // location.href,
+    params.get("url") || "train.splat",
+    "https://huggingface.co/cakewalk/splat-data/resolve/main/"
+  );
   const req = await fetch(url, {
-    mode: "cors",
-    credentials: "omit",
+    mode: "cors", // no-cors, *cors, same-origin
+    credentials: "omit", // include, *same-origin, omit
   });
-
   console.log(req);
   if (req.status != 200)
     throw new Error(req.status + " Unable to load " + req.url);
