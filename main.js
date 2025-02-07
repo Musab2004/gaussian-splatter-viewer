aa852d338b2db0e252e4e112f41282bfa451a6aa
Musab <musabbinabaid2004@gmail.com>
2025-02-07 12:45:35 +0500

    added my file for it

--- main.js
+++ main.js
@@ -726,16 +726,14 @@ async function main() {
     viewMatrix = JSON.parse(decodeURIComponent(location.hash.slice(1)));
     carousel = false;
   } catch (err) {}
-  const url = new URL(
-    // "nike.splat",
-    // location.href,
-    params.get("url") || "train.splat",
-    "https://huggingface.co/cakewalk/splat-data/resolve/main/"
-  );
+  const fileId = "1Gl6VRqnT9M_xO9lmAt2VhZmKCA0fs5xs";
+  const url = `https://drive.google.com/uc?export=download&id=${fileId}`;
+
   const req = await fetch(url, {
-    mode: "cors", // no-cors, *cors, same-origin
-    credentials: "omit", // include, *same-origin, omit
+    mode: "cors",
+    credentials: "omit",
   });
+
   console.log(req);
   if (req.status != 200)
     throw new Error(req.status + " Unable to load " + req.url);
